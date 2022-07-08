efa416bc8c6724cd3f593c26d212e1a99ae4c1d6
karthikianala <mohanakarthik.mk@gmail.com>
2022-07-08 12:20:04 +0530

    Uploading

---
+++ script.js
@@ -0,0 +1,8 @@
+var ln=Math.random()*20
+ln=Math.floor(ln);
+var guessing=document.getElementById("guessing");
+var check=document.getElementById("check");
+function checknum(){
+    var gn=guessing.ariaValueMax;
+}
+check.onclick=checknum;
